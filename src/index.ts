e920df25973efb654e8ee969f149c4fba7896176
alias-rahil <rahil.kabani.4@gmail.com>
2023-05-11 07:07:15 +0530

    fix: remove unneeded files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export {};
+import {createRequire} from 'node:module';
+
+type LibtorrentNode = {
+	hello: () => 'world';
+};
+
+const require = createRequire(import.meta.url);
+const hello = require('../../build/Release/libtorrentNode.node') as LibtorrentNode;
+
+export default hello;

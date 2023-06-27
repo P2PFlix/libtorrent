4ab4262b1b1f7b6cbf5700bd2c67f0861d3ee8ae
alias-rahil <rahil.kabani.4@gmail.com>
2023-06-28 04:30:40 +0530

    fix: variable reference scope

--- src/make_address.cpp
+++ src/make_address.cpp
@@ -2,6 +2,6 @@
 Napi::External<boost::asio::ip::address> Libtorrent::MakeAddress(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
-    boost::asio::ip::address address = libtorrent::make_address(info[0].As<Napi::String>().Utf8Value());
-    return Napi::External<boost::asio::ip::address>::New(env, &address);
+    boost::asio::ip::address *address = new boost::asio::ip::address(libtorrent::make_address(info[0].As<Napi::String>().Utf8Value()));
+    return Napi::External<boost::asio::ip::address>::New(env, address);
 };
